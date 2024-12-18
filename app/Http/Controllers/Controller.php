f1d04610dbed41710707be8d817d4468e474730b
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-18 11:24:37 -0300

    008

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function cleanUpperCaseString(string $string): string
+    {
+        // Remover os espaços em branco no início e fim de uma string
+        // Converter a string para uppercase (maiúsculas)
+        return strtoupper(trim($string));
+    }
 }

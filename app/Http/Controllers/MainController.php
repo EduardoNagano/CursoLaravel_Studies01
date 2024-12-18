45a46a44f81f24307aeb27d274a73632198bccbd
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-18 10:30:04 -0300

    005

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,43 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index()
+    public function initMethod(): string
     {
-        echo 'index';
+        return "Hello World";
     }
 
-    public function about()
+    public function viewPage(): View
     {
-        echo 'about';
-    }
-
-    public function mostrarValor($valor)
-    {
-        echo "Valor enviado pela rota: $valor";
-    }
-
-    public function mostrarValores($valor1, $valor2)
-    {
-        echo "Valores enviados pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValores2(Request $request, $valor1, $valor2)
-    {
-        echo "Valores enviados pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValorOpcional($valor = null) {
-        echo "Valor opcional: $valor";
-    }
-
-    public function mostrarValorOpcional2($valor1, $valor2 = 100) {
-        echo "Valor obrigatório: $valor1 e valor opcional: $valor2";
-    }
-
-    public function mostrarPosts($user_id, $post_id) {
-        echo "Posts do usuário ID: $user_id e post com ID: $post_id";
+        return view('home');
     }
 }

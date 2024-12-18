befbf533f6c8333db90e8b6076c0b5beaf2f171f
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-18 11:00:44 -0300

    006

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+// Route para Controller Single Action
+Route::get('/single', SingleActionController::class)->name('single');
+
+// Route para Controller Resource
+// Route::resource('/users', UserController::class);
+
+Route::resources([
+    'users' => UserController::class,
+    'clients' => ClientController::class,
+    'products' => ProductController::class
+]);
